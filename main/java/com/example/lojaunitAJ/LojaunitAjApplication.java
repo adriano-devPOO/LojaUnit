f176f61a7780db77c7d5ffc4740d6ed4f16d0440
adriano-devPOO <62193701+adriano-devPOO@users.noreply.github.com>
2020-11-02 19:07:16 -0300

    Projeto final pós graduação

---
+++ main/java/com/example/lojaunitAJ/LojaunitAjApplication.java
@@ -0,0 +1,13 @@
+package com.example.lojaunitAJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaunitAjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaunitAjApplication.class, args);
+	}
+
+}
